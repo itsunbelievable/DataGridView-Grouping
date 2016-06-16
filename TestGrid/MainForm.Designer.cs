286cb34e0c73e606fbcfcb05a8b546ad2ba22edf
Stanislav Kovyazin <stanislav.kovyazin@vizsc.com>
2016-06-16 12:18:03 +0300

    Initial commit

---
+++ TestGrid/MainForm.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace TestGrid
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nameBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dbDataSet = new TestGrid.dbDataSet();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.nameTA = new TestGrid.dbDataSetTableAdapters.nameTableAdapter();
+            this.toolsTA = new TestGrid.dbDataSetTableAdapters.toolsTableAdapter();
+            this.idDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.surnameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phoneDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.outlookGrid1 = new OutlookStyleControls.OutlookGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nameBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outlookGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.idDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.surnameDataGridViewTextBoxColumn,
+            this.dataDataGridViewTextBoxColumn,
+            this.phoneDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.nameBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(548, 585);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // nameBindingSource
+            // 
+            this.nameBindingSource.DataMember = "name";
+            this.nameBindingSource.DataSource = this.dbDataSet;
+            // 
+            // dbDataSet
+            // 
+            this.dbDataSet.DataSetName = "dbDataSet";
+            this.dbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Collapse all";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(103, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Expand all";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // nameTA
+            // 
+            this.nameTA.ClearBeforeFill = true;
+            // 
+            // toolsTA
+            // 
+            this.toolsTA.ClearBeforeFill = true;
+            // 
+            // idDataGridViewTextBoxColumn
+            // 
+            this.idDataGridViewTextBoxColumn.DataPropertyName = "id";
+            this.idDataGridViewTextBoxColumn.HeaderText = "id";
+            this.idDataGridViewTextBoxColumn.Name = "idDataGridViewTextBoxColumn";
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // surnameDataGridViewTextBoxColumn
+            // 
+            this.surnameDataGridViewTextBoxColumn.DataPropertyName = "surname";
+            this.surnameDataGridViewTextBoxColumn.HeaderText = "surname";
+            this.surnameDataGridViewTextBoxColumn.Name = "surnameDataGridViewTextBoxColumn";
+            // 
+            // dataDataGridViewTextBoxColumn
+            // 
+            this.dataDataGridViewTextBoxColumn.DataPropertyName = "data";
+            this.dataDataGridViewTextBoxColumn.HeaderText = "data";
+            this.dataDataGridViewTextBoxColumn.Name = "dataDataGridViewTextBoxColumn";
+            // 
+            // phoneDataGridViewTextBoxColumn
+            // 
+            this.phoneDataGridViewTextBoxColumn.DataPropertyName = "phone";
+            this.phoneDataGridViewTextBoxColumn.HeaderText = "phone";
+            this.phoneDataGridViewTextBoxColumn.Name = "phoneDataGridViewTextBoxColumn";
+            // 
+            // outlookGrid1
+            // 
+            this.outlookGrid1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.outlookGrid1.CollapseIcon = null;
+            this.outlookGrid1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.outlookGrid1.ExpandIcon = null;
+            this.outlookGrid1.Location = new System.Drawing.Point(598, 50);
+            this.outlookGrid1.Name = "outlookGrid1";
+            this.outlookGrid1.Size = new System.Drawing.Size(518, 585);
+            this.outlookGrid1.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1128, 647);
+            this.Controls.Add(this.outlookGrid1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nameBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outlookGrid1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private dbDataSet dbDataSet;
+        private dbDataSetTableAdapters.nameTableAdapter nameTA;
+        private dbDataSetTableAdapters.toolsTableAdapter toolsTA;
+        private System.Windows.Forms.BindingSource nameBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn idDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn surnameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn phoneDataGridViewTextBoxColumn;
+        private OutlookStyleControls.OutlookGrid outlookGrid1;
+    }
+}
+
---
+++ TestGrid/MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Windows.Forms;
+using DgvFilterPopup;
+using OutlookStyleControls;
+
+namespace TestGrid
+{
+    public partial class MainForm : Form
+    {
+        DataGridViewGrouper dgv;
+        public MainForm()
+        {
+            InitializeComponent();
+            dgv = new DataGridViewGrouper(dataGridView1);
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            dgv.CollapseAll();
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+            nameTA.Fill(this.dbDataSet.name);
+            DgvFilterManager filterManager = new DgvFilterManager(dataGridView1);
+            dgv.SetGroupOn("phone");
+
+            outlookGrid1.BindData(dbDataSet,"name");
+
+            outlookGrid1.GroupTemplate = new OutlookGridAlphabeticGroup();
+            outlookGrid1.GroupTemplate.Column = phoneDataGridViewTextBoxColumn;
+            outlookGrid1.GroupTemplate.Collapsed = true;
+            //used custom datarowcomparer
+            outlookGrid1.Sort(new DataRowComparer(4,ListSortDirection.Ascending));
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            dgv.ExpandAll();
+        }
+
+        public class DataRowComparer : IComparer
+        {
+            ListSortDirection direction;
+            int columnIndex;
+
+            public DataRowComparer(int columnIndex, ListSortDirection direction)
+            {
+                this.columnIndex = columnIndex;
+                this.direction = direction;
+            }
+
+            #region IComparer Members
+
+            public int Compare(object x, object y)
+            {
+
+                DataRow obj1 = (DataRow)x;
+                DataRow obj2 = (DataRow)y;
+                return string.Compare(obj1[columnIndex].ToString(), obj2[columnIndex].ToString()) * (direction == ListSortDirection.Ascending ? 1 : -1);
+            }
+            #endregion
+        }
+    }
+}
